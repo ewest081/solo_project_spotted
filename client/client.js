f78ff5b76eb940500f261daaaadc99cd9832d990
Liz Westberg <ewest081@gmail.com>
2016-02-01 16:31:43 -0600

    Hook up Spotted database to server and index

--- client/client.js
+++ client/client.js
@@ -26,5 +26,5 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
 //              App Controllers
 //[][][][][][][][][][][][][][][][][][][][][][][][][][]
 app.controller('MainController', ['$scope', function($scope){
-    $scope.message = "Hello World";
+    $scope.message = "Angularis working!";
 }]);
