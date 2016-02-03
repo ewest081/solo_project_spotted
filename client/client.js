6e8f741f510dd70f929e2b6aadfd073fdd39be92
Liz Westberg <ewest081@gmail.com>
2016-02-03 17:14:18 -0600

    Clean up commented out lines

--- client/client.js
+++ client/client.js
@@ -66,80 +66,63 @@ app.controller('MainController', ['$scope', 'userData', function($scope, userDat
         $scope.signedIn = false;
       }
     };
-}]);///no longer nesting controllers
+}]);
 
-    app.controller('HomeController', ['$scope', function($scope){
-        $scope.about = false;
-        $scope.toggleAbout = function(){
-          $scope.about = $scope.about === true ? false: true;
-        };
-    }]);
-
-
-
-    app.controller('AboutController', ['$scope', function($scope){
-
-    }]);
+app.controller('HomeController', ['$scope', function($scope){
+    $scope.about = false;
+    $scope.toggleAbout = function(){
+      $scope.about = $scope.about === true ? false: true;
+    };
+}]);
 
 
 
-    app.controller('SignInController', ['$scope', '$http', '$location', 'userData', function($scope, $http, $location, userData){
-      $scope.data = {};
+app.controller('AboutController', ['$scope', function($scope){
 
-      $scope.submitData = function(){
-        $http.post('/', $scope.data).then(function(response){
-          userData.setUser($scope.data.username);
-          console.log($scope.data.username);
-          $location.path(response.data);
-
-          // userData.sendData($scope.data);
-          // $scope.globalUser = $scope.data.username;
-          // changeNavLinks();
-        });
-        // console.log("SignInController: ", $scope.data.user);
-        // userData.id = $scope.data.user.id;
-        // userData.username = $scope.data.user.username;
-
-
-      };
-
-    }]);
+}]);
 
 
 
-    app.controller('RegisterController', ['$scope', function($scope){
+app.controller('SignInController', ['$scope', '$http', '$location', 'userData', function($scope, $http, $location, userData){
+  $scope.data = {};
 
-    }]);
+  $scope.submitData = function(){
+    $http.post('/', $scope.data).then(function(response){
+      userData.setUser($scope.data.username);
+      console.log($scope.data.username);
+      $location.path(response.data);
+    });
+  };
 
-
-
-    app.controller('GuestController', ['$scope', function($scope){
-
-    }]);
-
-
-
-    app.controller('UserController', ['$scope', 'userData', function($scope, userData){
-        console.log('hi:', userData.currentUser.username);
-        $scope.thisUser = userData.currentUser.username;
-
-        // $scope.$on('data_shared', function(){
-        //   var sharedData = userData.getData();
-        //   $scope.thisUser = sharedData;
-        // });
-        $scope.welcome = "Welcome, " + $scope.thisUser + "!";
-        // $scope.welcome = "Welcome, " + $scope.globalUser.username + "!";
-
-        console.log("UserController: ", userData.currentUser);
-    }]);
+}]);
 
 
 
-    app.controller('FailController', ['$scope', function($scope){
-      console.log("Ha ha!");
-    }]);
+app.controller('RegisterController', ['$scope', function($scope){
 
-// }]);
+}]);
+
+
+
+app.controller('GuestController', ['$scope', function($scope){
+
+}]);
+
+
+
+app.controller('UserController', ['$scope', 'userData', function($scope, userData){
+    console.log('hi:', userData.currentUser.username);
+    $scope.thisUser = userData.currentUser.username;
+
+    $scope.welcome = "Welcome, " + $scope.thisUser + "!";
+}]);
+
+
+
+app.controller('FailController', ['$scope', function($scope){
+  console.log("Ha ha!");
+}]);
+
 //[][][][][][][][][][][][][][][][][][][][][][][][][][]
 //              App Factories
 //[][][][][][][][][][][][][][][][][][][][][][][][][][]
@@ -148,28 +131,11 @@ app.factory('userData', ['$http', '$rootScope', '$timeout', function($http, $roo
   var currentUser = {
     username: ''
   };
-  // currentUser.data = false;
 
-  // currentUser.sendData = function(data){
-  //   this.data = data;
-  //   // $timeout(function(){
-  //     // $rootScope.$broadcast('data_shared');
-  //
-  //     // $http.get('/getUser').then(function(response){
-  //     //   this.data = response.data;
-  //     // });
-  //     // changeNavLinks();
-  //   // }, 100);
-  // };
-  // currentUser.getData = function(){
-  //   return this.data;
-  // };
-var setUser = function(username){
-  currentUser.username = username;
-};
+  var setUser = function(username){
+    currentUser.username = username;
+  };
 
-  // console.log("userData:", currentUser);
-  // return currentUser;
   return {
     currentUser: currentUser,
     setUser: setUser
