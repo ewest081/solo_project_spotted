789de7d4267c050aa4c9ec64cd41aa6b66bf22e9
Liz Westberg <ewest081@gmail.com>
2016-02-05 14:01:43 -0600

    More new controller files

--- client/client.js
+++ client/client.js
@@ -168,44 +168,11 @@ app.controller('EditProfileController', ['$scope', 'userData', function($scope, 
 }]);
 
 
-app.controller('ViewDataController', ['$scope', 'userData', function($scope, userData){
-  $scope.simpleList = false;
-  $scope.sortType = false;
-  $scope.keyword = false;
-  $scope.viewTable = false;
-
-  $scope.simpleListTog = function(){
-    $scope.simpleList = true;
-    $scope.sortType = false;
-    $scope.keyword = false;
-    $scope.viewTable = false;
-  };
-  $scope.sortTypeTog = function(){
-    $scope.sortType = true;
-    $scope.keyword = false;
-    $scope.viewTable = false;
-    $scope.simpleList = false;
-  };
-  $scope.keywordTog = function(){
-    $scope.keyword = true;
-    $scope.sortType = false;
-    $scope.simpleList = false;
-    $scope.viewTable = false;
-  };
-  $scope.viewTableTog = function(){
-    $scope.viewTable = true;
-    $scope.sortType = false;
-    $scope.keyword = false;
-    $scope.simpleList = false;
-  };
-
-}]);
-
-
 
 app.controller('EntrySuccessController', ['$scope', 'userData', function($scope, userData){
 
 }]);
+
 
 
 app.controller('LogOutController', ['$scope', 'userData', function($scope, userData){
